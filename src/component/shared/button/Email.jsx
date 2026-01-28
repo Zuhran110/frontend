ba9341db164a0dfd4dd73a0725dda3b71cf0f2c8
zuhran110 <35305@riphah.students.edu.pk>
2026-01-28 17:30:59 +0500

    email us

--- src/component/shared/button/Email.jsx
+++ src/component/shared/button/Email.jsx
@@ -4,6 +4,9 @@ const Email = () => {
   return (
     <Link
       href="/Email"
+      onClick={() => {
+        window.location.href = "mailto:youremail@gmail.com";
+      }}
       className="bg-transparent px-8 py-2  border text-custom-bg rounded-lg hidden md:flex"
     >
       Email Us
